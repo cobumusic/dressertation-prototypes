a286ae9e7dd1e1f18e9e8c85776f776a1231efea
cobumusic <jacobmugalde@gmail.com>
2024-04-04 17:50:24 -0700

    set up code and strings for spinners (dropdowns)

--- app/src/main/java/com/example/dressertationaddpage/MainActivity.java
+++ app/src/main/java/com/example/dressertationaddpage/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dressertationaddpage;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinnerGarmentType = (Spinner) findViewById(R.id.spinnerGarmentType);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapterGarmentType = ArrayAdapter.createFromResource(
+                this,
+                R.array.garmentTypes,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapterGarmentType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        spinnerGarmentType.setAdapter(adapterGarmentType);
+
+        Spinner spinnerWhereStored = (Spinner) findViewById(R.id.spinnerWhereStored);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapterWhereStored = ArrayAdapter.createFromResource(
+                this,
+                R.array.whereStoredOptions,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears.
+        adapterWhereStored.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        spinnerWhereStored.setAdapter(adapterWhereStored);
     }
+
 }
